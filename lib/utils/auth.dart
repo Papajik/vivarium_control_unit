93ec12a88fd7987f001fbe10f8fe562825152923
Michal Papaj <papaj.mich@gmail.com>
2020-09-14 13:27:29 +0200

    change code according to new flutter library version

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -15,30 +15,33 @@ String photonAccessToken;
 
 Future<String> signInWithGoogle() async {
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+
   final GoogleSignInAuthentication googleSignInAuthentication =
       await googleSignInAccount.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
+  final AuthCredential credential = GoogleAuthProvider.credential(
     accessToken: googleSignInAuthentication.accessToken,
     idToken: googleSignInAuthentication.idToken,
   );
 
-  final AuthResult authResult = await _auth.signInWithCredential(credential);
-  final FirebaseUser user = authResult.user;
+  final UserCredential authResult = await _auth.signInWithCredential(credential);
+
+  final User user = authResult.user;
+
 
   assert(user.email != null);
   assert(user.displayName != null);
-  assert(user.photoUrl != null);
+  assert(user.photoURL != null);
   assert(user.uid != null);
 
   userId = user.uid;
   name = user.displayName;
   email = user.email;
-  imageUrl = user.photoUrl;
-  IdTokenResult idtoken = await user.getIdToken();
+  imageUrl = user.photoURL;
+  IdTokenResult idToken = await user.getIdTokenResult();
 
   print("UserID = "+userId);
-  print("idtoken = "+idtoken.toString());
+  print("idToken = "+idToken.toString());
 
 
   if (name.contains(" ")) {
@@ -48,40 +51,39 @@ Future<String> signInWithGoogle() async {
   assert(!user.isAnonymous);
   assert(await user.getIdToken() != null);
 
-  final FirebaseUser currentUser = await _auth.currentUser();
+  final User currentUser = _auth.currentUser;
   assert(user.uid == currentUser.uid);
 
   photonSecret = await createOrGetPhotonSecret(userId);
   photonAccessToken = await getPhotonAccessToken(userId);
   print(photonSecret);
 
-
   return 'signInWithGoogle succeeded: $user';
 }
 
 Future<String> createOrGetPhotonSecret(String uid) async {
-  final ref = Firestore.instance.collection("users").document(uid);
+  final ref = FirebaseFirestore.instance.collection("users").doc(uid);
   String s = await ref.get().then((document) {
-    if (!document.data.containsKey("particleSecret")) {
+    if (!document.data().containsKey("particleSecret")) {
       String secret = randomAlpha(25);
-      ref.setData({
+      ref.set({
         "particleSecret": secret
       });
       return secret;
     } else {
-      return document["particleSecret"];
+      return document.data()["particleSecret"];
     }
   });
   return s;
 }
 
 Future<String> getPhotonAccessToken(String uid) async {
-  final ref = Firestore.instance.collection("users").document(uid);
+  final ref = FirebaseFirestore.instance.collection("users").doc(uid);
   String s = await ref.get().then((document) {
-    if (!document.data.containsKey("particle_access_token")) {
+    if (!document.data().containsKey("particle_access_token")) {
       return "";
     } else {
-      return document["particle_access_token"];
+      return document.data()["particle_access_token"];
     }
   });
   return s;
