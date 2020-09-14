c873a4940623905bcfd154744a87a99015382946
Michal Papaj <papaj.mich@gmail.com>
2020-09-14 13:57:46 +0200

    rework device UI

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -9,6 +9,14 @@ class Constants extends InheritedWidget {
   final String logoutText = "Log out";
   final String notSignedText = "Sign In";
   final String signInWarningText = "You need to sign in first";
+  final String bluetoothOffWarningText = "You need to turn bluetooth ON first";
+  final String bluetoothNoDevice = "Couldn't find device";
+  final String bleService = "FFE0";
+  final String bleCharacteristic = "FFE1";
+  final String deviceOverview = "Overview";
+  final String deviceSettings = "Settings";
+  final String deviceSaveSettings = "Save settings";
+  final String deviceView = "Camera";
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
