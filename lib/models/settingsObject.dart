3c288676f90ff667da4074431d9460553a7ba0c2
Michal Papaj <papaj.mich@gmail.com>
2020-09-14 13:53:47 +0200

    extend settings object

--- lib/models/settingsObject.dart
+++ lib/models/settingsObject.dart
@@ -1,51 +1,71 @@
+//import 'package:vivarium_control_unit/models/additionalInfo.dart';
 import 'package:vivarium_control_unit/models/ledTrigger.dart';
-import 'package:vivarium_control_unit/models/peripherals.dart';
+import 'package:vivarium_control_unit/models/peripheral.dart';
 
 class SettingsObject {
-  bool heaterAuto;
-  double tempGoal;
-  Peripherals peripherals;
+  // Peripherals peripherals;
+
+  double waterLevelSensorPosition;
+  double waterLevelMax;
+  double waterLevelMin;
+
   List<LedTrigger> ledTriggers;
+  int currentLedColor;
+
+  List<Peripheral> peripherals;
+
+  double waterTemperature;
+  double maxPh;
+  double minPh;
+
+  int time;
+
+  //AdditionalInfo additionalInfo;
 
   SettingsObject(
-      {this.peripherals, this.heaterAuto, this.tempGoal, this.ledTriggers});
+      {this.peripherals,
+      this.ledTriggers,
+      this.time,
+      this.currentLedColor,
+      this.maxPh,
+      this.minPh,
+      this.waterLevelMax,
+      this.waterLevelMin,
+      this.waterLevelSensorPosition,
+      this.waterTemperature});
 
   SettingsObject.fromJson(Map<String, dynamic> data)
       : this(
-            heaterAuto: data['heaterAuto'],
-            tempGoal: data['tempGoal'].toDouble(),
-            peripherals: Peripherals.fromJson(
-                Map<String, dynamic>.from(data['peripherals'])),
-            ledTriggers: createListOfTriggers(data['ledTriggers'])
-            // ledTriggers: json.decode(data['ledTriggers']).map((Map model) => LedTrigger.fromJson(data)),
-            /**
-        lightsInstructions: json
-        .decode(data['lights'])
-        .map((Map model) =>
-        {print(model), LightsInstruction.fromJSON(model)})
-        .toList()
-     */
-            );
+          peripherals: createListOfPeripherals(data['peripherals']),
+          ledTriggers: createListOfTriggers(data['ledTriggers']),
+          //additionalInfo: AdditionalInfo.fromJson(data['additionalInfo']);
+        );
 
   Map<String, dynamic> toJson() => {
-        "heaterAuto": heaterAuto,
-        "tempGoal": num.parse(tempGoal.toStringAsFixed(1)),
-        "peripherals": peripherals.toJson()
+        // "peripherals": peripheralsToJson(),
       };
-
-  Map<String, dynamic> toMap() {
-    return {
-      "heaterAuto": heaterAuto,
-      "tempGoal": num.parse(tempGoal.toStringAsFixed(1)),
-      "peripherals": peripherals.toJson(),
-    };
-  }
 
   static List<LedTrigger> createListOfTriggers(List data) {
     List<LedTrigger> list = new List();
     for (Map i in data) {
       list.add(LedTrigger.fromJson(Map<String, dynamic>.from(i)));
     }
+    print("settingsObject: createListOfTriggers");
+    list.forEach((item) {
+      print(item.toString());
+    });
     return list;
   }
+
+  static List<Peripheral> createListOfPeripherals(List data) {
+    List<Peripheral> list = new List();
+    for (Map i in data) {
+      list.add(Peripheral.fromJson(Map<String, dynamic>.from(i)));
+    }
+    print("settingsObject: createListOfPeripherals");
+    return list;
+  }
+
+  SettingsObject.newEmpty()
+      : this(peripherals: new List(), ledTriggers: new List());
 }
